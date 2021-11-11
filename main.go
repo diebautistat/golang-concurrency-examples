97a1d3bc3d4db538e128fbec7df5bdfad9207153
diebautistat <diego.bautistatorres@mercadolibre.com.co>
2021-11-11 09:34:35 -0500

    Third example: Buffered channels

--- main.go
+++ main.go
@@ -4,21 +4,11 @@ import (
 	"fmt"
 )
 
-func sum(numbers []int, channel chan int) {
-	sum := 0
-	for _, num := range numbers {
-		sum += num
-	}
-	channel <- sum
-}
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6}
-	channel := make(chan int)
-	go sum(numbers[:len(numbers)/2], channel)
-	go sum(numbers[len(numbers)/2:], channel)
-	fmt.Println("First gotten result", <- channel)
-	secondResult := <- channel
-	fmt.Println("Second gotten result", secondResult)
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
 
