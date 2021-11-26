27c1d84b58b1344c3548a44fd3a14816569bee15
diebautistat <diego.bautistatorres@mercadolibre.com.co>
2021-11-26 02:57:01 -0500

    Eleventh Example: Data sync using Mutexes.

--- main.go
+++ main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+type atomicCounter struct {
+	mut   sync.Mutex
+	count int
+}
+
+func (ac *atomicCounter) incrementBy(n int) {
+	ac.mut.Lock()
+	ac.count += n
+	ac.mut.Unlock()
+}
+
 func dataRace() {
-    var count int
+	counter := atomicCounter{}
 	done1 := make(chan bool)
-    done2 := make(chan bool)
+	done2 := make(chan bool)
 	go func() {
 		for i := 0; i < 1000; i++ {
-			count++ //non atomic operation
+			counter.incrementBy(1)
 		}
 		done1 <- true
 	}()
-    go func() {
+	go func() {
 		for i := 0; i < 1000; i++ {
-			count++ //non atomic operation
+			counter.incrementBy(1)
 		}
 		done2 <- true
 	}()
 	<-done1
-    <-done2
-	fmt.Println("The counter value is: ", count)
+	<-done2
+	fmt.Println("The counter value is: ", counter.count)
 }
 
 func main() {
-	for i := 0; i < 20; i++{
-        dataRace() //This might produce different outputs each run
-    }
+	for i := 0; i < 20; i++ {
+		dataRace() //This will produce the same output each run
+	}
 }
